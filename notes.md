f7dc250de0bdcfa5e5d7d77a5ee62459f6841ffc
Andrew Safigan <asafigan@gmail.com>
2023-01-20 19:25:38 +0900

    v1 example

---
+++ notes.md
@@ -0,0 +1,21 @@
+* talk about type mismatch in a vec
+example:
+
+error[E0308]: mismatched types
+  --> src/v1/handlers.rs:11:36
+   |
+11 |             children: vec![logo(), "Kansai Rust", menu()],
+   |                                    ^^^^^^^^^^^^^ expected opaque type, found `&str`
+...
+40 | fn logo() -> impl Component {
+   |              -------------- the expected opaque type
+   |
+   = note: expected opaque type `impl components::Component`
+                found reference `&'static str`
+
+* talk about manually casting to `Box<dyn Component>`
+example:
+
+`Box::new(H1("Title")) as Box<dyn Component>`
+
+* only the first element of vec needs `as Box<dyn Component>`
--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,24 @@
-use axum::{response::IntoResponse, routing::get, Router};
+mod v1;
+
+use axum::{
+    http::StatusCode,
+    response::IntoResponse,
+    routing::{get, get_service},
+    Router,
+};
+use tower_http::services::ServeDir;
 
 pub fn build_app() -> Router {
-    Router::new().route("/", get(index))
+    let serve_dir = get_service(ServeDir::new("public"));
+
+    Router::new()
+        .route("/", get(index))
+        .nest("/v1", v1::router())
+        .nest_service("/public", serve_dir.handle_error(handle_error))
+}
+
+async fn handle_error(_err: std::io::Error) -> impl IntoResponse {
+    (StatusCode::INTERNAL_SERVER_ERROR, "Something went wrong...")
 }
 
 async fn index() -> impl IntoResponse {
